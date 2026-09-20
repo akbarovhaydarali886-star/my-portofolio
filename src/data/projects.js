3617c2bb8cec5e49647b68c696f14d3338460b30
Haydarali <akbarovhaydarali886@gmail.com>
2026-09-20 16:20:10 +0500

    feat: add telegram bot project

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,5 +5,12 @@ export const projects = [
     description: 'Quduq qazish va toza ichimlik suvi qidirish xizmatlari uchun yaratilgan zamonaviy sayt (Landing Page).',
     link: 'https://kalodesweb.vercel.app/',
     image: '/kalodesweb.png'
+  },
+  {
+    id: '2',
+    title: 'Telegram Bot (Quduq Master)',
+    description: 'Quduq qazish xizmatlari uchun buyurtmalarni qabul qiluvchi qulay Telegram bot.',
+    link: 'https://t.me/kalodez_zakaz_bot',
+    image: '/kalodez-zakaz-bot.jpg'
   }
 ];
